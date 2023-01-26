7a07edfb80f30d79ed0a9fa6b220014bfdb8bcfb
khin-hla <119106104+khin-hla@users.noreply.github.com>
2023-01-26 19:29:52 +0630

    First month project

---
+++ app.js
@@ -0,0 +1,28 @@
+const reactNav = document.querySelector('.react-nav');
+const burger = document.querySelector('.bars')
+
+burger.addEventListener('click', () => {
+  reactNav.classList.toggle('open')
+})
+
+const navBtn=document.querySelector('.start-btn')
+const login=document.querySelector('.login-link')
+const navLogin=document.querySelector('.nav-login')
+const navButton=document.querySelector('.nav-button')
+
+
+navBtn.addEventListener('click', ()=> {
+  alert("Sorry! We don't provide this version yet.")
+})
+
+login.addEventListener('click', ()=> {
+  alert("Sorry! We don't provide this version yet.")
+})
+
+navLogin.addEventListener('click', ()=> {
+  alert("Sorry! We don't provide this version yet.")
+})
+
+navButton.addEventListener('click', ()=> {
+  alert("Sorry! We don't provide this version yet.")
+})
